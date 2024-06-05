e8356fcdff5c0d131ef2bb81ac320cc42d9498fe
Carr Chang <carr.z.chang@outlook.com>
2024-06-05 21:08:32 +0800

    Update description of git command

--- command/git.go
+++ command/git.go
@@ -1,23 +1,23 @@
 package command
 
 import (
-  "github.com/spf13/cobra"
+	"github.com/spf13/cobra"
 
-  "github.com/carrchang/handy-ci/execution"
+	"github.com/carrchang/handy-ci/execution"
 )
 
 var gitCommand = &cobra.Command{
-  Use:                "git",
-  Short:              "Execute Git command",
-  DisableFlagParsing: true,
-  Run: func(command *cobra.Command, args []string) {
-    execution.Execute(command, args, execution.GitExecution{})
-  },
+	Use:                "git",
+	Short:              "Execute git command",
+	DisableFlagParsing: true,
+	Run: func(command *cobra.Command, args []string) {
+		execution.Execute(command, args, execution.GitExecution{})
+	},
 }
 
 func init() {
-  rootCommand.AddCommand(gitCommand)
+	rootCommand.AddCommand(gitCommand)
 
-  gitCommand.PersistentFlags().SortFlags = false
-  gitCommand.Flags().SortFlags = false
+	gitCommand.PersistentFlags().SortFlags = false
+	gitCommand.Flags().SortFlags = false
 }
